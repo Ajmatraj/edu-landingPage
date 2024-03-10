e1b918955c47b5bfb3dbc6db9eff8cdf0a61ef58
Ajmatraj <159477674+Ajmatraj@users.noreply.github.com>
2024-03-10 13:59:55 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var buttons = document.querySelectorAll('.clickButtons');
+    buttons.forEach(function(button, index) {
+      button.addEventListener('click', function() {
+        var paragraphs = document.querySelectorAll('.textC');
+        paragraphs.forEach(function(paragraph) {
+          paragraph.style.display = 'none';
+        });
+        paragraphs[index].style.display = 'block';
+      });
+    });
+  });
+  
+
+
+//   dropdown 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+// review 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
